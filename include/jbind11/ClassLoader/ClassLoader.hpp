a50b6eb362b025b4f7709c350070911dd7c95c3d
Patrick Langer <planger@guests-als-1-155.intern.ethz.ch>
2023-10-06 16:35:30 +0200

    Added headers. Fixed crash due to env->FindClass.

--- include/jbind11/ClassLoader/ClassLoader.hpp
+++ include/jbind11/ClassLoader/ClassLoader.hpp
@@ -1,3 +1,23 @@
+/***************************************************************************
+* Copyright (C) 2023 ETH Zurich
+* Core AI & Digital Biomarker, Acoustic and Inflammatory Biomarkers (ADAMMA)
+* Centre for Digital Health Interventions (c4dhi.org)
+* 
+* Authors: Patrick Langer
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*         http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+***************************************************************************/
+
 #pragma once
 #include <jni.h>
 #include "JavaPackage/JavaPackageManager.hpp"
@@ -63,6 +83,18 @@ namespace jbind11
                 }
 
 
+                if(strlen(name) == 0)
+                {
+                    JBIND_THROW("Error, cannot find class, string is empty (provide a non-empty class name");
+                }
+                // This would cause FindClass to throw an unrecoverable exception..
+                if(!std::isalnum(name[strlen(name) - 1]))
+                {
+                    JBIND_THROW("Error, cannot find class \"" << name << "\". The specified class ends with non alpha-numeric charachter '"
+                    << name[strlen(name) - 1] << "', which is not allowed");
+                }
+
+
 
                 jstring str = env->NewStringUTF(name);
 
@@ -81,6 +113,8 @@ namespace jbind11
                 {
                     // If failed, try conventional method.
                     env->ExceptionClear();
+
+
                     cls = env->FindClass(name);
                 }
 
